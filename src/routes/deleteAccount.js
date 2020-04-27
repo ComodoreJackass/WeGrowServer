a263e577fb6d8cfd45affa9993a1917b444304d8
Ivan Puljic <ivan.puljic.48@outlook.com>
2020-04-27 14:56:29 +0200

    Deletion update

--- src/routes/deleteAccount.js
+++ src/routes/deleteAccount.js
@@ -3,18 +3,29 @@ import authenticateToken from '../Authentication/authenticate'
 
 const router = Router();
 
-router.delete('/', authenticateToken, async(req, res) => {
+router.delete('/', authenticateToken, async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
     //delete user
-    try{
+    try {
         const users = await req.context.models.Users.findOne({
-            where: {username: username, password:password}
+            where: { username: username, password: password }
         });
+        const userId = users.id;
+        const progressTracking = await req.context.models.ProgressTracking.findAll({
+            where: { user_id: userId }
+        });
+
+
+        if (progressTracking.length > 0) {
+            progressTracking.forEach(element => {
+                element.destroy();
+            });
+        }
         users.destroy();
         return res.sendStatus(200);
-    } catch(err) {
+    } catch (err) {
         console.log(err);
         return res.sendStatus(400);
     }
